14bdef1eb5feabc375b2cf82f286fa0016d1d3ee
Ivan Beldad <ivan@beldad.me>
2021-01-16 18:50:33 +0100

    feat(target): add the protocol implementations to the definition file

--- src/target/prioritizers/prioritizer-definition.ts
+++ src/target/prioritizers/prioritizer-definition.ts
@@ -3,10 +3,13 @@ import { Protocol } from 'src/target/models/protocol.enum';
 import { BasePrioritizer } from 'src/target/prioritizers/strategies/base-prioritizer';
 import { Prioritizable } from 'src/target/prioritizers/models/prioritizable.interface';
 import { PriorityDecorator } from 'src/target/prioritizers/models/priority-decorator';
-import { PassthroughPrioritizerDecorator } from 'src/target/prioritizers/strategies/passthrough-prioritizer.decorator';
 import { AvoidMechPrioritizerDecorator } from 'src/target/prioritizers/strategies/enemy-types/avoid-mech-prioritizer.decorator';
 import { PreferMechPrioritizerDecorator } from 'src/target/prioritizers/strategies/enemy-types/prefer-mech-prioritizer.decorator';
 import { ClosestEnemiesPrioritizerDecorator } from 'src/target/prioritizers/strategies/distance/closest-enemies-prioritizer.decorator';
+import { FurthestEnemiesPrioritizerDecorator } from 'src/target/prioritizers/strategies/distance/furthest-enemies-prioritizer.decorator';
+import { AssistAlliesPrioritizerDecorator } from 'src/target/prioritizers/strategies/allies/assist-allies-prioritizer.decorator';
+import { AvoidCrossfirePrioritizerDecorator } from 'src/target/prioritizers/strategies/allies/avoid-crossfire-prioritizer.decorator';
+import { OutOfRangePrioritizerDecorator } from 'src/target/prioritizers/strategies/distance/out-of-range-prioritizer.decorator';
 
 type PriorityDecoratorClass = new (p: Prioritizable) => PriorityDecorator;
 type ProtocolPrioritizer = { [key in Protocol]: PriorityDecoratorClass };
@@ -21,7 +24,7 @@ export class PrioritizerDefinition {
   /**
    * Define rules that will be applied everywhere, doesn't matter the protocol
    */
-  readonly defaultPrioritizers: PriorityDecoratorClass[] = [];
+  readonly defaultPrioritizers: PriorityDecoratorClass[] = [OutOfRangePrioritizerDecorator];
 
   /**
    * Define rules that will apply depending on protocol
@@ -34,8 +37,8 @@ export class PrioritizerDefinition {
     [Protocol.AVOID_MECH]: AvoidMechPrioritizerDecorator,
     [Protocol.PRIORITIZE_MECH]: PreferMechPrioritizerDecorator,
     [Protocol.CLOSEST_ENEMIES]: ClosestEnemiesPrioritizerDecorator,
-    [Protocol.FURTHEST_ENEMIES]: PassthroughPrioritizerDecorator,
-    [Protocol.ASSIST_ALLIES]: PassthroughPrioritizerDecorator,
-    [Protocol.AVOID_CROSSFIRE]: PassthroughPrioritizerDecorator,
+    [Protocol.FURTHEST_ENEMIES]: FurthestEnemiesPrioritizerDecorator,
+    [Protocol.ASSIST_ALLIES]: AssistAlliesPrioritizerDecorator,
+    [Protocol.AVOID_CROSSFIRE]: AvoidCrossfirePrioritizerDecorator,
   };
 }
